29aad31b81dae017be24ee8f2fd57ecc6db2e536
gaiakaa <giovanamcosta@outlook.com>
2026-07-17 18:43:01 -0300

    feat: create Item model class

---
+++ src/Item.java
@@ -0,0 +1,46 @@
+public class Item {
+
+    private int id;
+    private String name;
+    private int quantity;
+    private String category;
+
+    public Item(int id, String name, int quantity, String category){
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.category = category;
+    }
+
+    public int getId() {
+        return id;
+    }
+ 
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public int getQuantity(){
+        return quantity;
+    }
+
+    public void setQuantity(int quantity){
+        this.quantity = quantity;
+    }
+
+    public String getCategory(){
+        return category;
+    }
+
+    public void setCategory(String category){
+        this.category = category;
+    }
+}
